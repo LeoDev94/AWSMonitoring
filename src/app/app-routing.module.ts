f694b9df9b5231471785f14b1016c35b04da4256
LeoDev94 <u201510070@upc.edu.pe>
2021-04-14 01:36:56 -0500

    Se agregaron las rutas basicas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { EstadisticasComponent } from './estadisticas/estadisticas.component';
+import { ProyectoDetailComponent } from './proyecto-detail/proyecto-detail.component'
+
+const routes:Routes = [
+  {path: 'proyectos',component: ProyectosComponent},
+  {path: 'proyectos/:proyecto:id',component:EstadisticasComponent},
+  {path: 'estadisticas',component:ProyectoDetailComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { EstadisticasComponent } from './estadisticas/estadisticas.component';
+import { ProyectoDetailComponent } from './proyecto-detail/proyecto-detail.component'
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProyectosComponent,
+    EstadisticasComponent,
+    ProyectoDetailComponent
   ],
   imports: [
     BrowserModule,
