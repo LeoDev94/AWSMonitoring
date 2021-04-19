4849ba49b7e2f0b92fbbbfcbb438a3d20dadf427
LeoDev94 <u201510070@upc.edu.pe>
2021-04-19 11:55:07 -0500

    Primeras graficas

--- src/app/estadisticas/estadisticas.component.ts
+++ src/app/estadisticas/estadisticas.component.ts
@@ -1,4 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {NgxChartsModule } from '@swimlane/ngx-charts'
+
+var datos = [
+  {
+    "name": "Germany",
+    "value": 8940000
+  },
+  {
+    "name": "USA",
+    "value": 5000000
+  },
+  {
+    "name": "France",
+    "value": 7200000
+  },
+    {
+    "name": "UK",
+    "value": 6200000
+  }
+];
 
 @Component({
   selector: 'app-estadisticas',
