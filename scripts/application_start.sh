3116169f009ef5eccde8c9bb882c4f736b785365
Emanuel Gonzales Rebatta <emgore.1410@gmail.com>
2021-06-06 21:36:00 -0500

    add permissions

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,5 +2,5 @@
 set -e
 # Stop all servers and start the server as a daemon
 echo "STARTING APP"
-pm2 delete monitoring-app
-pm2 start "ng serve --host 0.0.0.0 --port 4200" --name "monitoring-app"
+sudo pm2 delete monitoring-app
+sudo pm2 start "ng serve --host 0.0.0.0 --port 4200" --name "monitoring-app"
