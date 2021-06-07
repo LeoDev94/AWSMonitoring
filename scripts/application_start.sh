1f2dc56fa1cdf23ed2836a296658326da134232d
Emanuel Gonzales Rebatta <emgore.1410@gmail.com>
2021-06-06 21:35:07 -0500

    only one pm2

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,4 +3,4 @@ set -e
 # Stop all servers and start the server as a daemon
 echo "STARTING APP"
 pm2 delete monitoring-app
-pm2 pm2 start "ng serve --host 0.0.0.0 --port 4200" --name "monitoring-app"
+pm2 start "ng serve --host 0.0.0.0 --port 4200" --name "monitoring-app"
