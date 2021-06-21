dcc922e5a666b8de422a9de4e71791cafe9f4efd
Emanuel Gonzales Rebatta <emgore.1410@gmail.com>
2021-06-20 23:11:13 -0500

    Fix script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,5 +2,5 @@
 set -e
 # Stop all servers and start the server as a daemon TEST
 echo "STARTING APP"
-pm2 delete frontend-app
+pm2 delete --silent frontend-app
 pm2 start "ng serve --host 0.0.0.0 --port 4200" --name "frontend-app"
