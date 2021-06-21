3dae9b7a6e5fb381b1f2633d4618dc5eeeb2ceb2
Emanuel Gonzales Rebatta <emgore.1410@gmail.com>
2021-06-20 23:05:00 -0500

    Test pipeline

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-# Stop all servers and start the server as a daemon
+# Stop all servers and start the server as a daemon TEST
 echo "STARTING APP"
 pm2 delete frontend-app
 pm2 start "ng serve --host 0.0.0.0 --port 4200" --name "frontend-app"
